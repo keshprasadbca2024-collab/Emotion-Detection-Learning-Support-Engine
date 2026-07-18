1539c9921d5667545346024357b7cadcf420f93e
keshprasadbca2024-collab <keshprasad.bca2024@iujaipur.edu.in>
2026-07-18 15:16:29 +0530

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+import streamlit as st
 
+st.set_page_config(page_title="Emotion Detection & Learning Support Engine")
+
+st.title("Emotion Detection & Learning Support Engine")
+
+field = st.selectbox(
+    "Select Field",
+    ["Student", "Teacher", "Developer", "Other"]
+)
+
+problem = st.text_area("Enter your problem")
+
+if st.button("Analyze"):
+    st.success("Analysis Completed")
+    st.write("Selected Field:", field)
+    st.write("Problem:", problem)
